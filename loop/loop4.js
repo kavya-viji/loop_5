05ac60851ba7c7953a03ac8248db27b507567192
Admin <kavyak6999@gmail.com>
2022-12-14 14:28:36 +0530

    switch

---
+++ loop/loop4.js
@@ -0,0 +1,22 @@
+var price = parseInt(prompt("Enter the price"));
+switch(true){
+
+case(price>=60000):
+	alert ("Buy IPhone Mobile");
+break;
+
+case(price>=10000):
+	alert ("Buy Android Mobile");
+break;
+
+default:
+	alert ("Buy Basic mobile");
+	
+	 
+	 
+	 
+	 
+	  
+	 
+	 
+}
